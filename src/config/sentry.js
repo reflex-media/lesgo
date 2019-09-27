5c71db7f060baccc552ce03fa84ae72d5ec41c80
Mohd Sufiyan <mosufy@gmail.com>
2019-09-27 23:50:38 +0800

    Removed serverless-plugin-optimize

--- src/config/sentry.js
+++ src/config/sentry.js
@@ -3,4 +3,6 @@ export default {
   dsn: process.env.SENTRY_DSN,
   // Minimal level for error reporting. Ref: https://github.com/winstonjs/winston#logging
   level: process.env.SENTRY_LEVEL || 'error',
+  // Track release version and for sourcemaps reference
+  release: process.env.SENTRY_RELEASE || 'master',
 };
--- tests/setupTest.js
+++ tests/setupTest.js
@@ -1,8 +1,13 @@
+import { logger } from '@reflex-media/lesgo/utils';
+
 // Add test-specific environment configurations
 process.env.APP_ENV = 'test';
 process.env.APP_DEBUG = true;
 process.env.AWS_ACCOUNT_REGION = 'ap-southeast-1';
 process.env.AWS_ACCOUNT_ID = '111111111111';
+
+// withMeta is initialized in the request middlewares, thus this is faked/mocked
+logger.withMeta = logger.child({ abc: 'asd' });
 
 // Mock AWS SDK
 jest.mock('aws-sdk', () => {
