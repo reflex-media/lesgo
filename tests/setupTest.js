63ff42f24e6ad2adfdad08033942a569ed7f071c
Sufiyan Rahmat <sufiyan@incube8.sg>
2019-10-04 17:32:19 +0800

    Updated to core framework 0.2.1

--- tests/setupTest.js
+++ tests/setupTest.js
@@ -7,39 +7,15 @@ process.env.AWS_ACCOUNT_REGION = 'ap-southeast-1';
 process.env.AWS_ACCOUNT_ID = '111111111111';
 process.env.SENTRY_BUNDLED = true;
 
-// Mock and silence all console logs
-console.log = jest.fn();
-console.debug = jest.fn();
-console.info = jest.fn();
-console.warn = jest.fn();
-console.error = jest.fn();
-
-// Mock AWS SDK
-jest.mock('aws-sdk', () => {
+jest.mock('lesgo/utils', () => {
   return {
-    SQS: jest.fn().mockImplementation(opts => {
-      return {
-        sendMessage: jest.fn().mockImplementation(params => {
-          return {
-            promise: jest.fn().mockImplementation(() => {
-              return new Promise(resolve => {
-                const response = {
-                  ResponseMetadata: {
-                    RequestId: 'RequestId',
-                  },
-                  MD5OfMessageBody: 'MD5OfMessageBody',
-                  MessageId: 'MessageId',
-                  mocked: {
-                    opts,
-                    params,
-                  },
-                };
-                resolve(response);
-              });
-            }),
-          };
-        }),
-      };
-    }),
+    dispatch: jest.fn(() => Promise.resolve({ MessageId: 'MessageId' })),
+    logger: {
+      error: jest.fn(),
+      warn: jest.fn(),
+      info: jest.fn(),
+      debug: jest.fn(),
+      log: jest.fn(),
+    },
   };
 });
