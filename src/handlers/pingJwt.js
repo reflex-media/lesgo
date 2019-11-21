2c7a60b6b988954294e60d3a06d95dae0f886ebb
Sufiyan Rahmat <sufiyan@incube8.sg>
2019-11-21 16:19:07 +0800

    Moved verifyJwtMiddleware to the framework; Added pingJwt handler as sample

---
+++ src/handlers/pingJwt.js
@@ -0,0 +1,19 @@
+import middy from 'middy';
+import httpMiddleware from 'Middlewares/httpMiddleware';
+import verifyJwtMiddleware from 'Middlewares/verifyJwtMiddleware';
+import { connectSentry } from 'Utils/sentry';
+import ping from 'Core/ping';
+import app from 'Config/app';
+
+connectSentry();
+
+const originalHandler = event => {
+  return ping(event.input);
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const handler = middy(originalHandler);
+
+handler
+  .use(verifyJwtMiddleware())
+  .use(httpMiddleware({ debugMode: app.debug }));
