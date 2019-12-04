3c152d482f0c8e55907b209d63fff353f8604cc6
Daison Cariño <daison12006013@gmail.com>
2019-12-04 13:51:57 +0800

    Add Coverage (#7)
    * Rename elasticSearch to elasticsearch ElasticSearch to Elasticsearch
    
    * Add coverage for User model

--- tests/utils/database.spec.js
+++ tests/utils/database.spec.js
@@ -1,21 +1,7 @@
-import UserInit from 'Models/User';
-import DatabaseConnection from 'Utils/database';
-
-const User = UserInit(DatabaseConnection());
+import User from 'Models/User';
 
 describe('test model', () => {
-  it('should be able to execute a query', async () => {
-    await User.findOne().then(user => {
-      expect(user.name).not.toBe('');
-      expect(user.email).not.toBe('');
-      expect(user.email_verified_at).not.toBe('');
-      expect(user.remember_token).not.toBe('');
-      expect(user.created_at).not.toBe('');
-      expect(user.updated_at).not.toBe('');
-    });
+  it('should return the table name', async () => {
+    expect(User.tableName).toBe('users');
   });
-});
-
-afterAll(() => {
-  User.sequelize.close();
 });
