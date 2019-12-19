7394c40b9af796c66d571c9d39f7f3543b0c483f
Sufiyan Rahmat <sufiyan@incube8.sg>
2019-12-19 17:24:44 +0800

    Using native mysql connection instead of using knex as an option

---
+++ src/handlers/samples/rawDb.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+import middy from 'middy';
+import httpMiddleware from 'Middlewares/httpMiddleware';
+import app from 'Config/app';
+import { connectSentry } from 'Utils/sentry';
+import { db, dbRead, connectDb } from 'Utils/database';
+
+connectDb();
+connectSentry();
+
+const originalHandler = async () => {
+  // Fetch data from READ HOST
+  const dataRead = await dbRead.query('SELECT * FROM users LIMIT 1');
+  await dbRead.end();
+
+  // Fetch data from WRITE HOST
+  const dataWrite = await db.query('SELECT * FROM users LIMIT 1');
+  await db.end();
+
+  return [{ dataRead, dataWrite }];
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const handler = middy(originalHandler);
+
+handler.use(httpMiddleware({ debugMode: app.debug }));
