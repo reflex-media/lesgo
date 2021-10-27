762f67f0a6b7ec7ddb214aac5afa8e96cd2594de
xXLXx <leo.lavarias@gmail.com>
2021-10-27 08:32:04 +0800

    refactor: update sample code for sqs implementation

--- src/handlers/samples/queueProcessor.js
+++ src/handlers/samples/queueProcessor.js
@@ -2,23 +2,39 @@ import middy from 'middy';
 import normalizeSQSMessageMiddleware from 'Middlewares/normalizeSQSMessageMiddleware';
 import { connectSentry } from 'Utils/sentry';
 import ErrorException from 'Exceptions/ErrorException';
-import { PING_QUEUE_PROCESSOR_SAMPLE_ERROR } from 'Constants/errorCodes';
+import {
+  PING_QUEUE_PROCESSOR_SAMPLE_ERROR,
+  ERROR_EMPTY_PARAMETER,
+} from 'Constants/errorCodes';
 
 connectSentry();
 
-const originalHandler = event => {
+const originalHandler = async event => {
   const { collection } = event;
 
-  if (collection[0].data === undefined || collection[0].data === null)
-    return collection;
+  if (collection[0].data === undefined || collection[0].data === null) {
+    throw new ErrorException(
+      'Sample processed queue is empty',
+      ERROR_EMPTY_PARAMETER,
+      400
+    );
+  }
 
-  if (collection[0].data['failed-queue'] === undefined) return collection;
+  const response = await Promise.all(
+    collection.map(async record => {
+      if (record.data['failed-queue'] === undefined) {
+        return record;
+      }
 
-  throw new ErrorException(
-    'Sample processed queue error',
-    PING_QUEUE_PROCESSOR_SAMPLE_ERROR,
-    400
+      throw new ErrorException(
+        'Sample processed queue error',
+        PING_QUEUE_PROCESSOR_SAMPLE_ERROR,
+        400
+      );
+    })
   );
+
+  return response;
 };
 
 // eslint-disable-next-line import/prefer-default-export
