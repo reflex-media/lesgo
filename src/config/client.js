1f66d7e3a38d3a54a4d3ce6ede29e5456e8ebd6a
xXLXx <leo.lavarias@gmail.com>
2022-07-23 14:16:24 +0800

    feat: update config to latest for 0.7.8

--- src/config/client.js
+++ src/config/client.js
@@ -5,8 +5,23 @@ export default {
    *--------------------------------------------------------------------------
    *
    * Here you may specify what header key to use to pass for client identificaiton.
+   * It can be passed through headers or through url parameters
    */
-  headerKey: 'x-client-id',
+  headerKeys: ['x-client-id', 'X-Client-Id'],
+
+  /*
+   *--------------------------------------------------------------------------
+   * Callback on Success
+   *--------------------------------------------------------------------------
+   *
+   * Here you may call a callback after a successful verification is confirmed
+   */
+  callback: async handler => {
+    handler.event.site = {
+      id: handler.event.platform,
+      ...this.clients[handler.event.site],
+    };
+  },
 
   /*
    *--------------------------------------------------------------------------
@@ -16,13 +31,26 @@ export default {
    * Here are each of the clients setup to have access to your application.
    * `key` property is used for external identification, while the key is used for internal.
    * Both `key` and `secret` are used for Basic authentication.
-   * Other user-defined propoerties can defined as well for access under `handler.event.platform`, when a match exists.
+   *
+   * `isAuthOptional` propoerty can be passed as well, which skips authentication whenever basic auth is not provided,
+   * and only throws an authentication error when a basic auth is provided with incorrect credentials
+   *
+   * Other user-defined propoerties can defined as well for access when a match exists
+   * ```
+   * import client from 'Config/client';
+   *
+   * console.log(client[handler.event.platform]);
+   * ```
    */
   clients: {
     myApp: {
       key: process.env.MY_APP_KEY,
       secret: process.env.MY_APP_SECRET,
       some: 'value',
+    },
+    myPublicApp: {
+      key: 'public-app',
+      isAuthOptional: true,
     },
   },
 };
