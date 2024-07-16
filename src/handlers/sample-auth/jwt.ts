b5625aa12c652bc50dad6bf1b44f397b508e28bf
Sufiyan Rahmat <mosufy@gmail.com>
2024-07-17 02:45:30 +0800

    Sample JWT Auth (without validate claims)

---
+++ src/handlers/sample-auth/jwt.ts
@@ -0,0 +1,24 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import middy from '@middy/core';
+import { httpMiddleware, verifyJwtMiddleware } from 'lesgo/middlewares';
+import { logger } from 'lesgo/utils';
+import ping from '../../core/utils/ping';
+import appConfig from '../../config/app';
+
+const FILE = 'handlers.sample-auth.jwt';
+
+const pingHandler = (event: APIGatewayProxyEvent) => {
+  logger.debug(`${FILE}::RECEIVED_REQUEST`, event);
+
+  const res = ping(event.queryStringParameters!);
+  logger.debug(`${FILE}::RESPONSE_SENT`, res);
+
+  return res;
+};
+
+export const handler = middy()
+  .use(httpMiddleware({ debugMode: appConfig.debug }))
+  .use(verifyJwtMiddleware({ validateClaims: false }))
+  .handler(pingHandler);
+
+export default handler;
