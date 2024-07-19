6b6acd01942335b9432055cbf9505216adcd9bee
Sufiyan Rahmat <mosufy@gmail.com>
2024-07-20 07:08:28 +0800

    Sample getSecretsValue

---
+++ src/handlers/sample-secretsManager/getSecretValue.ts
@@ -0,0 +1,22 @@
+import middy from '@middy/core';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { httpMiddleware } from 'lesgo/middlewares';
+import { validateFields } from 'lesgo/utils';
+import { getSecretValue } from 'lesgo/utils/secretsManager';
+
+const getSecretValueHandler = async (event: APIGatewayProxyEvent) => {
+  const { queryStringParameters } = event;
+
+  const input = validateFields({ ...queryStringParameters! }, [
+    { key: 'secretValue', type: 'string', required: true },
+  ]);
+
+  const resp = await getSecretValue(input.text);
+  return resp;
+};
+
+export const handler = middy()
+  .use(httpMiddleware())
+  .handler(getSecretValueHandler);
+
+export default handler;
